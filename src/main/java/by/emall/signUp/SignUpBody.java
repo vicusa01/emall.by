c3e2ed19f2f5186238af832f733ae6a8d593631c
viktoriya-tsygler-idf <viktoriya.tsygler@idftechnology.by>
2025-03-06 21:29:21 +0300

    add SignBody.class

---
+++ src/main/java/by/emall/signUp/SignUpBody.java
@@ -0,0 +1,52 @@
+package by.emall.signUp;
+
+public class SignUpBody {
+    private String name;
+    private String surname;
+    private String patronymic;
+    private String phone;
+    private String email;
+    private String password;
+    private int[] target_ids;
+
+    public SignUpBody(String name, String surname, String patronymic, String phone, String email, String password, int[] target_ids) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.target_ids = target_ids;
+    }
+
+    public SignUpBody() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int[] getTarget_ids() {
+        return target_ids;
+    }
+}
