8bd6873dbb8a4c028290257103a3ad7f20370118
viktoriya-tsygler-idf <viktoriya.tsygler@idftechnology.by>
2025-03-06 21:31:30 +0300

    add errorMessages.class for Cart requests

---
+++ src/main/java/by/emall/cart/ErrorMessages.java
@@ -0,0 +1,6 @@
+package by.emall.cart;
+
+public class ErrorMessages {
+    public static final String INCORRECT_OFFER_ID = "Выбранное значение для \"offer id\" некорректно.";
+    public static final String ITEM_NOT_FOUND_IN_BASKET = "Товар не найден в корзине";
+}
