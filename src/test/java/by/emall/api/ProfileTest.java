02f7716c6a51baafe877d3d63bbf6f4a6aaf4d4c
vicusa01 <tzygler2015@yandex.com>
2025-03-14 00:05:32 +0300

    add api tests for profile

---
+++ src/test/java/by/emall/api/ProfileTest.java
@@ -0,0 +1,37 @@
+package by.emall.api;
+
+import by.emall.api.profile.Profile;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class ProfileTest {
+    @BeforeAll
+    public static void getToken() {
+        Token.getTokenAndCookies();
+    }
+
+    @Test
+    @DisplayName("View profile")
+    public void testViewProfile() {
+        Profile.getViewProfileRequest()
+                .then()
+                .statusCode(200)
+                .log()
+                .all()
+                .body("user.id", equalTo(4441203));
+    }
+
+    @Test
+    @DisplayName("Edit personal info")
+    public void testEditPersonalInfo() {
+        Profile.putEditPersonalInfoRequest("Вкаукв", "Цыглер", "Вал", "tet@mail.ru", "+375294567898")
+                .then()
+                .statusCode(200)
+                .log()
+                .all()
+                .body("id", equalTo(4441203));
+    }
+}
