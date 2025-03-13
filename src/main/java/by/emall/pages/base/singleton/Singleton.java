9a9d2fbdcca0cbbecb28ee4520912e821020dad3
vicusa01 <tzygler2015@yandex.com>
2025-03-14 00:15:29 +0300

    add method for getting currenUrl

---
+++ src/main/java/by/emall/pages/base/singleton/Singleton.java
@@ -0,0 +1,41 @@
+package by.emall.pages.base.singleton;
+
+import by.emall.pages.base.utils.WaitUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Singleton {
+
+    private static WebDriver driver;
+
+    private Singleton() {
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+
+            WebDriver.Options options = driver.manage();
+            WebDriver.Timeouts timeouts = options.timeouts();
+            timeouts.implicitlyWait(Duration.ofSeconds(10));
+            driver
+                    .manage()
+                    .window()
+                    .maximize();
+        }
+        return driver;
+    }
+
+    public static void quit() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+    public static String getUrl() {
+        WaitUtil.sleep(1500);
+        return driver.getCurrentUrl();
+    }
+}
