c609fa480cbb17fda4c21e1d6152ca7d54b3e579
vicusa01 <tzygler2015@yandex.com>
2025-03-14 00:18:38 +0300

    refactor code structory

--- src/main/java/by/emall/logIn/ErrorMessages.java
+++
@@ -1,8 +0,0 @@
-package by.emall.logIn;
-
-public class ErrorMessages extends by.emall.signUp.ErrorMessages {
-    public static final String INVALID_CREDENTIALS = "Неверный логин или пароль";
-    public static final String UNEXIST_PHONE = "Пожалуйста, убедитесь, что правильно ввели телефон";
-    public static final String WRONG_SMS_CODE = "Неверный код";
-    public static final String INVALID_TOKEN = "Передан невалидный токен";
-}
