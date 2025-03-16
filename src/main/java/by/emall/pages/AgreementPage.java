8abe76716ff152734ab3443ca8fc53077dad979f
vicusa01 <tzygler2015@yandex.com>
2025-03-16 20:30:39 +0300

    add class AgreementPage

---
+++ src/main/java/by/emall/pages/AgreementPage.java
@@ -0,0 +1,25 @@
+package by.emall.pages;
+
+import by.emall.pages.base.singleton.Singleton;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AgreementPage extends SignUpPage {
+    private WebDriver driver;
+
+    public AgreementPage() {
+        this.driver = Singleton.getDriver();
+    }
+
+    private final By BUTTON_NEXT_LOCATOR = By.xpath("(//span[text()='Далее'])[2]");
+    private final By BUTTON_ACCEPT_ALL_LOCATOR = By.xpath("(//i[@class='icon-check-16 icon_icon__HiJyg checkbox_checkbox__icon__E8Iw1'])[9]");
+    private final By BUTTON_ACCEPT_AGREEMENT_LOCATOR = By.xpath("//span[text()='Согласен']");
+
+    public SignUpPage acceptAgreement() {
+        driver.findElement(BUTTON_NEXT_LOCATOR).click();
+        driver.findElement(BUTTON_ACCEPT_ALL_LOCATOR).click();
+        driver.findElement(BUTTON_ACCEPT_AGREEMENT_LOCATOR).click();
+        return new SignUpPage();
+    }
+
+}
